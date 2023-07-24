e5eb36a1735818aa19717c87493d0e34d3af0cfb
Abel Ciccoli <abelcoli@outlook.fr>
2023-07-24 09:40:09 +0200

    feat(initialisation):  initialisation du projet spring

---
+++ src/main/java/com/example/springgo/SpringGoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGoApplication.class, args);
+    }
+
+}
